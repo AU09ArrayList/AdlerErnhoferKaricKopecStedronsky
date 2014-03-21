f804deea7812298dd8fd3cd814471517f26acbb4
Philipp Adler <padler@student.tgm.ac.at>
2014-03-21 10:44:48 +0100

    JUnit Testklasse erzeugt

---
+++ test/JUnitTest.java
@@ -0,0 +1,114 @@
+import org.junit.*;
+public class JUnitTest {
+
+	@Test
+	public void MyArrayList(){
+		
+	}
+
+	@Test
+	public void MyArrayList1(){
+
+	}
+	
+	@Test 
+	public void add(){
+		
+	}
+	
+	@Test
+	public void add1(){
+		
+	}
+	
+	@Test
+	public void clear(){
+		
+	}
+	
+	@Test
+	public void clonee(){
+		
+	}
+	
+	@Test
+	public void contains(){
+		
+	}
+	
+	@Test
+	public void ensureCapacity(){
+		
+	}
+	
+	@Test
+	public void get(){
+		
+	}
+	
+	@Test
+	public void isEmpty(){
+		
+	}
+	
+	@Test
+	public void indexOf(){
+		
+	}
+	
+	@Test
+	public void lastIndexOf(){
+		
+	}
+	
+	@Test
+	public void remove(){
+		
+	}
+	
+	@Test
+	public void remove1(){
+		
+	}
+	
+	@Test
+	public void removeRange(){
+		
+	}
+	
+	@Test
+	public void set(){
+		
+	}
+	
+	@Test
+	public void size(){
+		
+	}
+	
+	@Test
+	public void toArray(){
+		
+	}
+	
+	@Test
+	public void trimToSize(){
+		
+	}
+	
+	@Test
+	public void iterator(){
+		
+	}
+	
+	@Test
+	public void listIterator(){
+		
+	}
+	
+	@Test
+	public void listIterator1(){
+		
+	}
+
+}
